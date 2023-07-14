a7e253a07f0eb7169756b798d7f60f48815555f0
AnshulGupta54 <guptaanshul169@gmail.com>
2023-07-14 17:35:09 +0530

    second day commit

--- src/container/dashboard/components/Body/Bottomleft.js
+++ src/container/dashboard/components/Body/Bottomleft.js
@@ -1,11 +1,23 @@
-import classes from './Bottomleft.module.css';
+import classes from "./Bottomleft.module.css";
 
-const Bottomleft=()=>{
-    return (
-        <div className={classes.bottomleft}>
-            <p>Monthly Training Activity</p>
+const Bottomleft = () => {
+  return (
+    <div className={classes.bottomleft}>
+      <p>Monthly Training Activity</p>
+      <div>
+        <div>
+          <input type="radio" value="Male" name="gender" /> Course account
+          <p>23 workers took this course this week</p>
         </div>
-    );
- }
+        <div>
+          <input type="radio" value="Female" name="gender" /> Course b
+        </div>
+        <div>
+          <input type="radio" value="Other" name="gender" /> Course c
+        </div>
+      </div>
+    </div>
+  );
+};
 
- export default Bottomleft; 
+export default Bottomleft;
--- src/container/dashboard/components/Body/Bottommiddle.js
+++ src/container/dashboard/components/Body/Bottommiddle.js
@@ -1,11 +1,29 @@
-import classes from './Bottommiddle.module.css';
+import DonutChart from "react-donut-chart";
+import classes from "./Bottommiddle.module.css";
 
-const Bottommiddle=()=>{
-    return (
-        <div className={classes.bottommiddle}>
-            <p>Quiz Passing %</p>
-        </div>
-    );
- }
+const Bottommiddle = () => {
+  return (
+    <div className={classes.bottommiddle}>
+      <p>Quiz Passing %</p>
+      <DonutChart
+        data={[
+          {
+            label: "Passed",
+            value: 1423,
+          },
+          {
+            label: "Failed",
+            value: 134,
+          },
+        ]}
+        colors={['#018E42','#ed1c24']}
+        innerRadius={0.4}
+        outerRadius={0.7}
+        height={400}
+        width={400}
+      />
+    </div>
+  );
+};
 
- export default Bottommiddle; 
+export default Bottommiddle;
